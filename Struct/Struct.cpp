fc3ababbcc58929e028f58be403e129bf46930ba
aivylv <lyvialarasati@gmail.com>
2024-04-02 10:48:26 +0700

    Penginputan data

--- Struct/Struct.cpp
+++ Struct/Struct.cpp
@@ -12,4 +12,12 @@ struct Mahasiswa	//Membuat struktur mahasiswa untuk menyimpan data data dari see
 int main()
 {
 	Mahasiswa mhs;					//membuat objek/variabel untuk mewakili struktur mahasiswa
+	cout << "Nomor Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa : ";
+	cin >> mhs.alamat;
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
 }
